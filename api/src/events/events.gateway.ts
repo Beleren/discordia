9d2c92c52991920eaa6afbedd9bb1589b9256b60
Leandro Haruki Futema <leandroharuki@gmail.com>
2021-05-08 18:44:51 -0300

    chore(api): socket config

---
+++ api/src/events/events.gateway.ts
@@ -0,0 +1,19 @@
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+
+@WebSocketGateway()
+export class EventsGateway {
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('events')
+  async identity(@MessageBody() data: any): Promise<any> {
+    console.log('chegou', data);
+    return data;
+  }
+}
