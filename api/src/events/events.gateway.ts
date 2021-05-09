a21a0ecc0c2aed43556c87dfd0a61fb920b66391
Leandro Haruki Futema <leandroharuki@gmail.com>
2021-05-08 21:31:21 -0300

    feat(api): adding rooms

--- api/src/events/events.gateway.ts
+++ api/src/events/events.gateway.ts
@@ -1,19 +1,31 @@
+import { Logger } from '@nestjs/common';
 import {
+  ConnectedSocket,
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class EventsGateway {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('events')
-  async identity(@MessageBody() data: any): Promise<any> {
-    console.log('chegou', data);
-    return data;
+  logger = new Logger(EventsGateway.name);
+
+  @SubscribeMessage('join')
+  join(@MessageBody() room: string, @ConnectedSocket() client: Socket) {
+    this.logger.debug('connecting to room: ' + room);
+    client.join(room);
+    this.server.to(room).emit('message', 'welcome');
+  }
+
+  @SubscribeMessage('message')
+  identity(@MessageBody() payload: { to: string; message: string }) {
+    this.logger.debug('message received: ' + payload.message);
+    this.server.to(payload.to).emit('message', payload.message);
+    this.logger.debug('message sent to: ' + payload.to);
   }
 }
