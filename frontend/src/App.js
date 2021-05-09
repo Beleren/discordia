89ef1bdfe9753dcdbebe74a281677dc63700a86a
Leandro Haruki Futema <leandroharuki@gmail.com>
2021-05-08 21:47:46 -0300

    feat(frontend): chat section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,13 @@ import { useCallback, useContext, useEffect, useState } from 'react';
 function App() {
   const socket = useContext(SocketContext);
   const [userMessage, setUserMessage] = useState('');
+  const [chatMessages, setChatMessages] = useState([]);
   const [room, setRoom] = useState('teste');
   const handleMessage = useCallback((message) => {
-    console.log(message);
+    setChatMessages((prevMessages) => {
+      if (prevMessages.length >= 50) prevMessages.pop();
+      return [...prevMessages, message];
+    });
   }, []);
 
   useEffect(() => {
@@ -22,28 +26,42 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' width={50} />
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            if (userMessage.length > 0) {
-              socket.emit('message', { to: room, message: userMessage });
-              setUserMessage('');
-            }
-          }}
-        >
-          <input
-            value={userMessage}
-            onChange={({ target: { value } }) => setUserMessage(value)}
-          />
-          <button type='submit'>Send</button>
-        </form>
-        <button
+        <div style={{ width: '50vw' }}>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              if (userMessage.length > 0) {
+                socket.emit('message', { to: room, message: userMessage });
+                setUserMessage('');
+              }
+            }}
+          >
+            <input
+              value={userMessage}
+              onChange={({ target: { value } }) => setUserMessage(value)}
+            />
+            <button type='submit'>Send</button>
+          </form>
+          <div
+            style={{
+              border: '1px solid black',
+              height: '40vh',
+              overflowY: 'auto'
+            }}
+          >
+            {chatMessages.map((chatMessage) => (
+              <p style={{ fontSize: '12px' }}>{chatMessage}</p>
+            ))}
+          </div>
+        </div>
+
+        {/* <button
           onClick={() => {
             socket.emit('join', room);
           }}
         >
           Join test room
-        </button>
+        </button> */}
       </header>
     </div>
   );
