4eaa930032315e9ba8ef4a2be1201aaa9e11db76
Leandro Haruki Futema <leandroharuki@gmail.com>
2021-05-10 01:15:12 -0300

    chore(api): draft bot command listener and parser

--- api/src/events/events.gateway.ts
+++ api/src/events/events.gateway.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { HttpService, Logger } from '@nestjs/common';
 import {
   ConnectedSocket,
   MessageBody,
@@ -7,9 +7,23 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import * as papa from 'papaparse';
+import { ReadStream } from 'fs';
 
+type Stock = {
+  symbol: string;
+  date: string;
+  time: string;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+};
 @WebSocketGateway()
 export class EventsGateway {
+  constructor(private readonly httpService: HttpService) {}
+
   @WebSocketServer()
   server: Server;
 
@@ -23,9 +37,32 @@ export class EventsGateway {
   }
 
   @SubscribeMessage('message')
-  identity(@MessageBody() payload: { to: string; message: string }) {
-    this.logger.debug('message received: ' + payload.message);
+  async identity(@MessageBody() payload: { to: string; message: string }) {
     this.server.to(payload.to).emit('message', payload.message);
-    this.logger.debug('message sent to: ' + payload.to);
+    // BOT
+    if (payload.message.match(/^\/stock=/)) {
+      const stockingResponse = await this.httpService
+        .get<ReadStream>('http://localhost:8082')
+        .toPromise();
+
+      papa.parse(stockingResponse.data, {
+        header: true,
+        skipEmptyLines: true,
+        transformHeader: col =>
+          col
+            .trim()
+            .toLowerCase()
+            .replace(/\s/g, '_'),
+        complete: results => {
+          const [stock] = results.data as Stock[];
+          this.server
+            .to(payload.to)
+            .emit(
+              'message',
+              `${stock.symbol} quote is $${stock.close} per share`,
+            );
+        },
+      });
+    }
   }
 }
