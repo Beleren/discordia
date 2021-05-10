001c9f0fe21a44c82dee2106ad178f86386083a9
Leandro Haruki Futema <leandroharuki@gmail.com>
2021-05-10 02:04:45 -0300

    feat(stock): parse data to csv

--- stock/src/app.controller.ts
+++ stock/src/app.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpStatus,
+  NotFoundException,
+  Param,
+} from '@nestjs/common';
+import * as papa from 'papaparse';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  db = {
+    ['appl.us']: [
+      {
+        symbol: 'APPL.US',
+        date: new Date(2021, 4, 7, 0, 0, 0, 0),
+        time: '22:00:04',
+        open: 130.85,
+        high: 131.2582,
+        low: 129475,
+        close: 130.21,
+        volume: 78845855,
+      },
+    ],
+  };
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(':stockId')
+  @HttpCode(HttpStatus.OK)
+  @Header('Content-Type', 'application/csv')
+  @Header('Content-Disposition', 'attachment; filename=test.csv')
+  getStock(@Param('stockId') stockId: string) {
+    const stock = this.db[stockId];
+    if (!stock) throw new NotFoundException();
+    return papa.unparse(stock);
   }
 }
